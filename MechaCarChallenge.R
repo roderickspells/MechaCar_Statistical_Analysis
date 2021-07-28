7420c50edb137b768bc0cd55c44310489481cf4d
roderickspells <59187034+roderickspells@users.noreply.github.com>
2021-07-28 11:52:49 -0500

    deliverable 2

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,11 @@
+#deliverable 1
 library(dplyr) #load library
 MechaCarData <- read.csv(file = 'MechaCar_mpg.csv') #import csv
 mpg_regression <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCarData) #linear regression
 summary(mpg_regression) #summary of p-vale and r-square
+
+#deliverable 2
+SuspensionCoilData <- read.csv("Suspension_Coil.csv") #import csv
+total_summary <- summarize(SuspensionCoilData,Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+lot_summary <- SuspensionCoilData %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MechaCar_Statistical_Analysis
+
+## Linear Regression to Predict MPG
+![image](https://github.com/roderickspells/MechaCar_Statistical_Analysis/blob/main/Images/linear%20regression.png)
