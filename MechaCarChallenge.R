5bbfc2048295c2acac13729e2fd71a92422b5635
roderickspells <59187034+roderickspells@users.noreply.github.com>
2021-07-28 12:44:20 -0500

    deliverable 3

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,11 +1,20 @@
 #deliverable 1
+
 library(dplyr) #load library
 MechaCarData <- read.csv(file = 'MechaCar_mpg.csv') #import csv
 mpg_regression <- lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data = MechaCarData) #linear regression
 summary(mpg_regression) #summary of p-vale and r-square
 
 #deliverable 2
+
 SuspensionCoilData <- read.csv("Suspension_Coil.csv") #import csv
-total_summary <- summarize(SuspensionCoilData,Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
-lot_summary <- SuspensionCoilData %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI))
+total_summary <- summarize(SuspensionCoilData,Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) #create total summary
+lot_summary <- SuspensionCoilData %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI),Median=median(PSI),Variance=var(PSI),SD=sd(PSI)) #create lot summary
+
+#deliverable 3
+
+t.test(SuspensionCoilData$PSI,mu=1500) #t-test for all lots
+t.test(subset(SuspensionCoilData,Manufacturing_Lot=="Lot1")$PSI,mu=1500) #t-test for lot 1
+t.test(subset(SuspensionCoilData,Manufacturing_Lot=="Lot2")$PSI,mu=1500) #t-test for lot 2
+t.test(subset(SuspensionCoilData,Manufacturing_Lot=="Lot3")$PSI,mu=1500) #t-test for lot 3
 
--- README.md
+++ README.md
@@ -1,4 +1,41 @@
 # MechaCar_Statistical_Analysis
 
+## Software and Techniques 
+R \
+RStudio \
+Dplyr Library \
+Tidyverse Library
+
+
+
+
 ## Linear Regression to Predict MPG
+
+
+- Which variables/coefficients provided a non-random amount of variance to the mpg values in the dataset?
+
+- Is the slope of the linear model considered to be zero? Why or why not?
+
+- Does this linear model predict mpg of MechaCar prototypes effectively? Why or why not?
+
+### Linear Regression Results
+
 ![image](https://github.com/roderickspells/MechaCar_Statistical_Analysis/blob/main/Images/linear%20regression.png)
+
+##Summary Statistics on Suspension Coils
+
+- The design specifications for the MechaCar suspension coils dictate that the variance of the suspension coils must not exceed 100 pounds per square inch. Does the current manufacturing data meet this design specification for all manufacturing lots in total and each lot individually? Why or why not?
+
+### Total Summary
+
+![image]
+(https://github.com/roderickspells/MechaCar_Statistical_Analysis/blob/main/Images/total_summary.png)
+
+### Lot Summary 
+![image]
+(https://github.com/roderickspells/MechaCar_Statistical_Analysis/blob/main/Images/lot_summary.png)
+
+## T-Test on Suspension Coils
+
+
+## Study Desing: MechaCar vs. Competition
